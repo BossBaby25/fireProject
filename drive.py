f9d805ba8a8cb5398584d9fd523116633ae9df92
BossBaby25 <108459353+BossBaby25@users.noreply.github.com>
2022-10-07 16:33:20 +0600

    Create drive.py

---
+++ drive.py
@@ -0,0 +1,59 @@
+<p>import RPi.GPIO as GPIO
+ 
+# Use GPIO numbers not pin numbers
+GPIO.setmode(GPIO.BCM)
+ 
+# set up the GPIO channels - one input and one output
+GPIO.setup(7, GPIO.IN)
+GPIO.setup(8, GPIO.OUT)
+ 
+# input from GPIO7
+input_value = GPIO.input(7)
+ 
+# output to GPIO8
+GPIO.output(8, True)</p>
+motor.py
+
+# Import required libraries
+import sys
+import time
+import RPi.GPIO as GPIO
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+#GPIO.setmode(GPIO.BCM)
+mode=GPIO.getmode()
+print " mode ="+str(mode)
+GPIO.cleanup()
+
+# Define GPIO signals to use
+# Physical pins 11,15,16,18
+# GPIO17,GPIO22,GPIO23,GPIO24
+
+StepPinForward=16
+StepPinBackward=18
+sleeptime=1
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(StepPinForward, GPIO.OUT)
+GPIO.setup(StepPinBackward, GPIO.OUT)
+
+def forward(x):
+    GPIO.output(StepPinForward, GPIO.HIGH)
+    print "forwarding running  motor "
+    time.sleep(x)
+    GPIO.output(StepPinForward, GPIO.LOW)
+
+def reverse(x):
+    GPIO.output(StepPinBackward, GPIO.HIGH)
+    print "backwarding running motor"
+    time.sleep(x)
+    GPIO.output(StepPinBackward, GPIO.LOW)
+
+print "forward motor "
+forward(5)
+print "reverse motor"
+reverse(5)
+
+print "Stopping motor"
+GPIO.cleanup()
